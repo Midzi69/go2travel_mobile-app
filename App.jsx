752184cdbff07717cb5a3d54a12b4acd63bd237e
Miloš Tordai <milostordai@gmail.com>
2023-09-06 02:50:43 +0200

    First Commit

---
+++ App.jsx
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import StackNavigator from './StackNavigator';
+import { ModalPortal } from 'react-native-modals';
+import store from './store';
+import { Provider } from 'react-redux';
+export default function App() {
+  return (
+    <>
+      <Provider store={store}>
+
+     
+      <StackNavigator />
+      <ModalPortal />
+      </Provider>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});
